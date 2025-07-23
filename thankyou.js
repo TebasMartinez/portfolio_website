9c2c7ddb9d833ce2f4d3326c827824146b6a7c10
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-07-23 23:40:55 +0200

    Add JavaScript for thank you message

---
+++ thankyou.js
@@ -0,0 +1,17 @@
+let params = new URLSearchParams(document.location.search);
+let name = params.get("name");
+
+if (name) {
+    document.getElementById("thankyou").textContent = `Thank you for your message, ${name}!`;
+}
+else {
+    document.getElementById("thankyou").textContent = `Thank you for your message!`;
+}
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  const username = document.getElementById("name").value.trim();
+  const nextInput = document.getElementById("next-url");
+
+  const encodedName = encodeURIComponent(username);
+  nextInput.value = `https://tebasmartinez.github.io/portfolio_website/thankyou.html?name=${encodedName}`;
+});
