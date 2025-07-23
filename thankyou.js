729b27d6fcd94f99d63955ffad6be04ef7bcae85
Tebas Martínez <203688711+TebasMartinez@users.noreply.github.com>
2025-07-23 23:49:05 +0200

    Update thankyou js

--- thankyou.js
+++ thankyou.js
@@ -1,19 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-let params = new URLSearchParams(document.location.search);
-let name = params.get("name");
+    let params = new URLSearchParams(document.location.search);
+    let name = params.get("name");
 
-if (name) {
-    document.getElementById("thankyou").textContent = `Thank you for your message, ${name}!`;
-}
-else {
-    document.getElementById("thankyou").textContent = `Thank you for your message!`;
-}
+    if (name) {
+        document.getElementById("thankyou").textContent = `Thank you for your message, ${name}!`;
+    }
+    else {
+        document.getElementById("thankyou").textContent = `Thank you for your message!`;
+    }
 });
 
-document.querySelector("form").addEventListener("submit", function (e) {
-  const username = document.getElementById("name").value.trim();
-  const nextInput = document.getElementById("next-url");
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector("form").addEventListener("submit", function (e) {
+    const username = document.getElementById("name").value.trim();
+    const nextInput = document.getElementById("next-url");
 
-  const encodedName = encodeURIComponent(username);
-  nextInput.value = `https://tebasmartinez.github.io/portfolio_website/thankyou.html?name=${encodedName}`;
+    const encodedName = encodeURIComponent(username);
+    nextInput.value = `https://tebasmartinez.github.io/portfolio_website/thankyou.html?name=${encodedName}`;
+    });
 });
